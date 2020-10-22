ecc06ea549e4dd402667f468829a2fcaa4a8b93f
Annisa Purbandari <a.purbandari@gmail.com>
2020-10-22 14:57:38 +1100

    fix return value and pass test

--- Develop/app.js
+++ Develop/app.js
@@ -6,6 +6,10 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+
 const render = require("./lib/htmlRenderer");
 let employeesArr = []
 
